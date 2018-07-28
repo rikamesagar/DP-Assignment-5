b3cf1d544d8b8ae8adf2ea1d43d4fe31e9680003
pratikHan <patrick.hanchate@gmail.com>
2018-07-28 17:26:23 -0400

    implement Results and Stdinterface

---
+++ pratik_hanchate_assign_5/wordCount/src/wordCount/util/Results.java
@@ -0,0 +1,83 @@
+package wordCount.util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
+
+public class Results implements StdoutDisplayInterface,FileDisplayInterface{
+
+
+	FileWriter filewriter;
+	BufferedWriter bw;
+	//private String TAG="Results";
+	public String outputresult;
+	private static final int TAG=2;
+	
+	MyLogger logger =new MyLogger(TAG);
+
+	public Results(String outputfile, String outputstring) {
+		try {
+						 
+			filewriter=new FileWriter(outputfile);
+			bw= new BufferedWriter(filewriter);
+			this.outputresult=outputstring;
+			
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(TAG+"Exception in File writing");
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	@Override
+	public void writeToStdout(String s) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(s);
+		
+	}
+
+
+	@Override
+	public void writeToFile(String s) {
+		// TODO Auto-generated method stub
+	
+		try {
+			
+			bw.write(s);
+		//**	MyLogger.writeMessage("Write to file",DebugLevel.READ);
+						
+			} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(TAG+"Exception in write to File");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
+	}
+
+
+	
+	
+
+
+
+}
---
+++ pratik_hanchate_assign_5/wordCount/src/wordCount/util/StdoutDisplayInterface.java
@@ -0,0 +1,5 @@
+package wordCount.util;
+
+public interface StdoutDisplayInterface {
+	public void writeToStdout(String s);
+}
