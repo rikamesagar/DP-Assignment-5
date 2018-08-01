3f662c9dc9d2fbfc2c6321eb7940095009422d5a
rikamesagar <31829665+rikamesagar@users.noreply.github.com>
2018-07-31 20:41:35 -0400

    command line validation

--- pratik_hanchate_assign_5/wordCount/src/wordCount/driver/Driver.java
+++ pratik_hanchate_assign_5/wordCount/src/wordCount/driver/Driver.java
@@ -6,46 +6,48 @@ import wordCount.util.MyLogger;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String inputFile = null;
+		String outputFile = null;
+		int noOfIterations;
+		/**
+		 *Code to check the files if/ if not passed through arguments
+		 */
+		try {
+			inputFile = args[0];
+			if (!(inputFile.equals("input.txt"))) {
+				System.out.println("Input File not found, please check!");
+				System.exit(0);
+			}
+		} catch (Exception e) {
+			System.out.println("Output File not Found, please check!");
+			System.exit(0);
+		}
+		try {
+			outputFile = args[1];
+			if (!(outputFile.equals("delete.txt"))) {
+				System.out.println("Delete File not found, please check!");
+				System.exit(0);
+			}
+		} catch (Exception e) {
+			System.out.println("No file present to delete values from Nodes");
+			System.exit(0);
+		}
+		try {
+			noOfIterations = Integer.parseInt(args[2]);
+		}
+		} catch(NumberFormatException e) {
 
-		String inputfile="input.txt";
-		
-		String outputfile="output.txt";
-	
-		
-		
-		if(!checkArguments(args)) {
-			
-			throw new RuntimeException("Invalid number of arguments");
+			System.out.println("No. Of Iterations is not a number.");
+            System.exit(0);
 		}
-		
-		MyLogger logger =new MyLogger(1);
-		
-		
-		FileProcessor fileprocessor =new FileProcessor(inputfile);
-		
-		
-		
-		
-	}
-	
-	
-	
-public static boolean checkArguments(String arg[]) {
-		
-		if(arg.length<=2) {
-			return false;
-		}
-		
-		int var=Integer.parseInt(arg[2]);
-		
-		
-		if(var>3|| var<0) {
-			return  false;
-		}
-		
-		return true ;
-		
-	}
 
+		MyLogger logger = new MyLogger(1);
+
+
+			FileProcessor fileprocessor = new FileProcessor(inputfile);
+
+
+		}
+
+	}
 }
